525eee72c732b52527e888c3aa09e5de1a45184e
Epicnicity322 <christianorangel322@gmail.com>
2019-12-03 18:55:23 -0300

    Add Command Support to The API

---
+++ src/main/java/com/epicnicity322/epicpluginapi/command/Command.java
@@ -0,0 +1,49 @@
+package com.epicnicity322.epicpluginapi.command;
+
+import org.bukkit.command.CommandSender;
+
+public interface Command
+{
+    /**
+     * @return The name of the command.
+     */
+    String getName();
+
+    /**
+     * The minimum of arguments this command need to be executed.
+     *
+     * @return The minimum of arguments.
+     */
+    default int minArgsAmount()
+    {
+        return 1;
+    }
+
+    /**
+     * @return If this command is case sensitive.
+     */
+    default boolean isCaseSensitive()
+    {
+        return false;
+    }
+
+    /**
+     * When the command is successfully sent by the user.
+     *
+     * @param label  The name of the main command.
+     * @param sender The user.
+     * @param args   The arguments sent by the user.
+     */
+    void onCommand(String label, CommandSender sender, String[] args);
+
+    /**
+     * When the command is sent by the user, but it does not match the number of arguments required.
+     *
+     * @param label  The name of the main command.
+     * @param sender The user.
+     * @param args   The arguments sent by the user.
+     */
+    default void onNotEnoughArgs(String label, CommandSender sender, String[] args)
+    {
+    }
+}
---
+++ src/main/java/com/epicnicity322/epicpluginapi/command/CommandHandler.java
@@ -0,0 +1,86 @@
+package com.epicnicity322.epicpluginapi.command;
+
+import com.sun.istack.internal.Nullable;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class CommandHandler implements CommandExecutor
+{
+    private HashMap<String, Command> subCommands;
+    private Command description;
+
+    private CommandHandler(HashSet<Command> subCommands, Command description)
+    {
+        this.subCommands = new HashMap<>();
+
+        for (Command s : subCommands) {
+            this.subCommands.put(s.getName(), s);
+        }
+
+        this.description = description;
+    }
+
+    public static CommandHandler getInstance(HashSet<Command> subCommands)
+    {
+        return new CommandHandler(subCommands, null);
+    }
+
+    public static CommandHandler getInstance(HashSet<Command> subCommands, @Nullable Command description)
+    {
+        return new CommandHandler(subCommands, description);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, org.bukkit.command.Command command, String s, String[] strings)
+    {
+        if (strings.length == 0) {
+            if (description != null) {
+                description.onCommand(s, commandSender, strings);
+            }
+            return true;
+        }
+
+        Command command1 = findSubCommand(strings[0]);
+
+        if (command1 != null) {
+            if (strings.length >= command1.minArgsAmount()) {
+                command1.onCommand(s, commandSender, strings);
+            } else {
+                command1.onNotEnoughArgs(s, commandSender, strings);
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Loops through all sub commands and checks if one has the same name as the argument. If so, then it checks if the
+     * sub command is case sensitive and checks again if the argument matches case.
+     *
+     * @param arg The argument to check.
+     * @return The command if the argument matches.
+     */
+    private Command findSubCommand(String arg)
+    {
+        for (String cmdName : subCommands.keySet()) {
+            Command command = subCommands.get(cmdName);
+
+            if (cmdName.equalsIgnoreCase(arg)) {
+                if (command.isCaseSensitive()) {
+                    if (cmdName.equals(arg)) {
+                        return command;
+                    }
+                } else {
+                    return command;
+                }
+            }
+        }
+
+        return null;
+    }
+}
