3c39934b5e85cd60e8e00c4ccde9c60c8a593e11
Epicnicity322 <christianorangel322@gmail.com>
2019-12-13 03:31:17 -0300

    Reformatted code

--- src/main/java/com/epicnicity322/epicpluginapi/command/CommandManager.java
+++ src/main/java/com/epicnicity322/epicpluginapi/command/CommandManager.java
@@ -2,6 +2,7 @@ package com.epicnicity322.epicpluginapi.command;
 
 import com.sun.istack.internal.Nullable;
 import org.bukkit.command.PluginCommand;
+
 import java.util.HashSet;
 
 public class CommandManager
@@ -13,7 +14,8 @@ public class CommandManager
      * @param subCommands The arguments to be called when the user uses them.
      * @see CommandManager#registerCommand(PluginCommand, HashSet, Command)
      */
-    public static void registerCommand(PluginCommand mainCommand, HashSet<Command> subCommands) {
+    public static void registerCommand(PluginCommand mainCommand, HashSet<Command> subCommands)
+    {
         mainCommand.setExecutor(CommandHandler.getInstance(subCommands));
     }
 
@@ -24,7 +26,8 @@ public class CommandManager
      * @param subCommands The arguments to be called when the user uses them.
      * @param description When the command has 0 arguments, this will be called.
      */
-    public static void registerCommand(PluginCommand mainCommand, HashSet<Command> subCommands, @Nullable Command description) {
+    public static void registerCommand(PluginCommand mainCommand, HashSet<Command> subCommands, @Nullable Command description)
+    {
         mainCommand.setExecutor(CommandHandler.getInstance(subCommands, description));
     }
 }
