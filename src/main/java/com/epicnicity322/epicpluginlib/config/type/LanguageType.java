25712ad7e6d25167865302a32e7f29d422e72c67
christianornj <christianorangel322@gmail.com>
2020-04-17 02:04:04 -0300

    Removed

--- src/main/java/com/epicnicity322/epicpluginlib/config/type/LanguageType.java
+++
@@ -1,8 +0,0 @@
-package com.epicnicity322.epicpluginlib.config.type;
-
-public enum LanguageType implements Type
-{
-    EXTERNAL,
-    HARD_CODED,
-    JAR
-}
--- src/main/java/com/epicnicity322/epicpluginlib/config/type/Type.java
+++
@@ -1,8 +0,0 @@
-package com.epicnicity322.epicpluginlib.config.type;
-
-/**
- * Just to generalize all types into one interface.
- */
-public interface Type
-{
-}
