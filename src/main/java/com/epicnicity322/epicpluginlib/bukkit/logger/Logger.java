8f52c2280739b4fbd2048de446fa1c402bbab01d
Epicnicity322 <christianorangel322@gmail.com>
2020-06-17 18:11:23 -0300

    Implement core logger interface.

--- src/main/java/com/epicnicity322/epicpluginlib/bukkit/logger/Logger.java
+++ src/main/java/com/epicnicity322/epicpluginlib/bukkit/logger/Logger.java
@@ -19,6 +19,7 @@
 
 package com.epicnicity322.epicpluginlib.bukkit.logger;
 
+import com.epicnicity322.epicpluginlib.core.logger.ConsoleLogger;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
@@ -27,7 +28,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.logging.Level;
 import java.util.regex.Pattern;
 
-public class Logger
+public class Logger implements ConsoleLogger<CommandSender>
 {
     private static final @NotNull Pattern formatCodes = Pattern.compile("&[a-fk-o0-9r]");
     private final @NotNull String prefix;
@@ -37,33 +38,22 @@ public class Logger
         this.prefix = prefix;
     }
 
-    /**
-     * Logs formatted messages with the prefix to console.
-     *
-     * @param message The message with color codes to send to console.
-     */
+    @Override
+    public @NotNull String getPrefix()
+    {
+        return prefix;
+    }
+
     public void log(@NotNull String message)
     {
         log(Bukkit.getConsoleSender(), message);
     }
 
-    /**
-     * Removes color codes the from message and logs to console with a specific {@link Level} and the prefix using bukkit's
-     * {@link java.util.logging.Logger}.
-     *
-     * @param message The message to log to console.
-     */
     public void log(@NotNull String message, @NotNull Level level)
     {
         Bukkit.getLogger().log(level, formatCodes.matcher(message).replaceAll(""));
     }
 
-    /**
-     * Sends formatted messages with the prefix to the {@link CommandSender}.
-     *
-     * @param sender  Who the message will be sent.
-     * @param message The message to be sent.
-     */
     public void log(@NotNull CommandSender sender, @NotNull String message)
     {
         sender.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + message));
