34a3fed8559e3b09d377f7b4c60c8e323bbe69a0
Epicnicity322 <christianorangel322@gmail.com>
2020-06-21 23:09:42 -0300

    Using Version objects to compare versions.

--- src/main/java/com/epicnicity322/epicpluginlib/bukkit/updater/Updater.java
+++ src/main/java/com/epicnicity322/epicpluginlib/bukkit/updater/Updater.java
@@ -20,9 +20,8 @@
 package com.epicnicity322.epicpluginlib.bukkit.updater;
 
 import com.epicnicity322.epicpluginlib.core.tools.Downloader;
-import com.epicnicity322.epicpluginlib.core.util.StringUtils;
+import com.epicnicity322.epicpluginlib.core.tools.Version;
 import org.bukkit.Bukkit;
-import org.bukkit.plugin.PluginDescriptionFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -39,53 +38,23 @@ import java.nio.charset.StandardCharsets;
 public class Updater
 {
     private final @NotNull File jar;
-    private final @NotNull String currentVersion;
-    private String latestVersion;
+    private final @NotNull Version currentVersion;
+    private Version latestVersion;
     private boolean hasUpdate = false;
     private URL VERSION_URL;
     private URL DOWNLOAD_URL;
 
     /**
-     * Creates a instance of Updater using your plugin's jar file, the current version and the id in spigotmc.org.
-     * The jar is used to name the downloaded file, in case you want to download one.
-     * The version is used to check if it's greater than the latest one in spigotmc.org.
-     * The id is used to check for version and find download links in spigotmc.org. You can identify the id by your
-     * plugin's URL. Basically, every plugin URL should look like "https://www.spigotmc.org/resources/<name>.<id>". We
-     * want the thing after the dot.
+     * Creates an Updater that can check and download updates on spigotmc.org.
      *
      * @param jar            Your plugin's jar, you can get it by JavaPlugin#getFile().
-     * @param currentVersion The current version of your plugin. Only numbers and dots allowed!
+     * @param currentVersion The current version of your plugin.
      * @param id             The id of your plugin in spigotmc.org.
      */
-    public Updater(@NotNull File jar, @NotNull String currentVersion, int id)
+    public Updater(@NotNull File jar, @NotNull Version currentVersion, int id)
     {
         this.jar = jar;
         this.currentVersion = currentVersion;
-
-        try {
-            VERSION_URL = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + id);
-            DOWNLOAD_URL = new URL("https://api.spiget.org/v2/resources/" + id + "/download");
-        } catch (MalformedURLException ignored) {
-        }
-    }
-
-    /**
-     * Creates a instance of Updater using your plugin's jar file, the current version and the id in spigotmc.org.
-     * The jar is used to name the downloaded file, in case you want to download one.
-     * The description file is used to get your plugin's current version, so it can check if the latest version in
-     * spigotmc.org is greater than your plugin version.
-     * The id is used to check for version and find download links in spigotmc.org. You can identify the id by your
-     * plugin's URL. Basically, every plugin URL should look like "https://www.spigotmc.org/resources/<name>.<id>". We
-     * want the thing after the dot.
-     *
-     * @param jar             Your plugin's jar, you can get it by JavaPlugin#getFile().
-     * @param descriptionFile Your plugin's plugin.yml file. In case your version contains only numbers and dots.
-     * @param id              The id of your plugin in spigotmc.org.
-     */
-    public Updater(@NotNull File jar, @NotNull PluginDescriptionFile descriptionFile, int id)
-    {
-        this.jar = jar;
-        this.currentVersion = descriptionFile.getVersion();
 
         try {
             VERSION_URL = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + id);
@@ -109,9 +78,9 @@ public class Updater
             if (downloader.getResult() != Downloader.Result.SUCCESS)
                 return CheckResult.valueOf(downloader.getResult().toString());
 
-            latestVersion = new String(baos.toByteArray(), StandardCharsets.UTF_8);
+            latestVersion = new Version(new String(baos.toByteArray(), StandardCharsets.UTF_8));
 
-            if (StringUtils.isVersionGreater(latestVersion, currentVersion)) {
+            if (latestVersion.compareTo(currentVersion) > 0) {
                 hasUpdate = true;
                 return CheckResult.AVAILABLE;
             } else {
@@ -129,7 +98,6 @@ public class Updater
             File update = Bukkit.getUpdateFolderFile();
 
             if (update.mkdirs()) {
-
                 Downloader downloader = new Downloader(DOWNLOAD_URL, new FileOutputStream(new File(update,
                         jar.getName())));
                 Thread thread = new Thread(downloader, "Update Downloader");
@@ -150,12 +118,12 @@ public class Updater
         return Downloader.Result.UNEXPECTED_ERROR;
     }
 
-    public @Nullable String getLatestVersion()
+    public @Nullable Version getLatestVersion()
     {
         return latestVersion;
     }
 
-    public @NotNull String getCurrentVersion()
+    public @NotNull Version getCurrentVersion()
     {
         return currentVersion;
     }
