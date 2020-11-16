1412a97ea301b04226467a3ab24133533bfc1b4c
Epicnicity322 <christianorangel322@gmail.com>
2020-11-16 10:49:44 -0300

    Change version to 1.6.6

--- src/main/java/com/epicnicity322/epicpluginlib/core/EpicPluginLib.java
+++ src/main/java/com/epicnicity322/epicpluginlib/core/EpicPluginLib.java
@@ -29,7 +29,7 @@ public interface EpicPluginLib
     /**
      * The version of EpicPluginLib as string.
      */
-    @NotNull String versionString = "1.6.5";
+    @NotNull String versionString = "1.6.6";
 
     /**
      * The version of EpicPluginLib.
