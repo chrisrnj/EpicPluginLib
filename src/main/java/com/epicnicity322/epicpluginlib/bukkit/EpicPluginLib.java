35e23349f26dd89d77fd604a4d7689e7402ab895
Epicnicity322 <christianorangel322@gmail.com>
2021-05-29 17:33:43 -0300

    Change license to GNU as of v2.0

--- src/main/java/com/epicnicity322/epicpluginlib/bukkit/EpicPluginLib.java
+++ src/main/java/com/epicnicity322/epicpluginlib/bukkit/EpicPluginLib.java
@@ -1,20 +1,19 @@
 /*
- * Copyright (c) 2020 Christiano Rangel
+ * EpicPluginLib - Library with basic utilities for bukkit plugins.
+ * Copyright (C) 2021  Christiano Rangel
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package com.epicnicity322.epicpluginlib.bukkit;
@@ -25,7 +24,7 @@ import com.epicnicity322.epicpluginlib.core.config.ConfigurationLoader;
 import com.epicnicity322.epicpluginlib.core.logger.ConsoleLogger;
 import com.epicnicity322.epicpluginlib.core.tools.SpigotUpdateChecker;
 import com.epicnicity322.epicpluginlib.core.tools.Version;
-import org.bstats.bukkit.MetricsLite;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
@@ -101,11 +100,9 @@ public final class EpicPluginLib extends JavaPlugin implements com.epicnicity322
         try {
             // bStats libraries were added in 1.8.3, testing if metrics should run.
             Class.forName("com.google.gson.JsonElement");
-            MetricsLite metrics = new MetricsLite(this, 8337);
+            Metrics metrics = new Metrics(this, 8337);
 
-            if (metrics.isEnabled()) {
-                logger.log("EpicPluginLib is using bStats as metrics collector.");
-            }
+            logger.log("EpicPluginLib is using bStats as metrics collector.");
         } catch (ClassNotFoundException ignored) {
         }
     }
