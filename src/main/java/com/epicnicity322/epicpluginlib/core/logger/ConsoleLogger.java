2fd4fa872d6f5dcf490a60d135890bc757e929d7
Epicnicity322 <christianorangel322@gmail.com>
2022-10-08 11:21:04 -0300

    Add simpleLogger method

--- src/main/java/com/epicnicity322/epicpluginlib/core/logger/ConsoleLogger.java
+++ src/main/java/com/epicnicity322/epicpluginlib/core/logger/ConsoleLogger.java
@@ -1,6 +1,6 @@
 /*
  * EpicPluginLib - Library with basic utilities for bukkit plugins.
- * Copyright (C) 2021  Christiano Rangel
+ * Copyright (C) 2022  Christiano Rangel
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,6 +27,47 @@ import org.jetbrains.annotations.NotNull;
  */
 public interface ConsoleLogger<R>
 {
+    /**
+     * Creates a simple, platform-independent logger. This logger should be used as a placeholder, you should use your
+     * platform's {@link ConsoleLogger} implementing class as soon as it's available.
+     *
+     * @param prefix The prefix of every message.
+     */
+    static @NotNull ConsoleLogger<?> simpleLogger(@NotNull String prefix)
+    {
+        return new ConsoleLogger<Object>()
+        {
+            @Override
+            public @NotNull String getPrefix()
+            {
+                return prefix;
+            }
+
+            @Override
+            public void log(@NotNull String message)
+            {
+                log(message, Level.INFO);
+            }
+
+            @Override
+            public void log(@NotNull String message, @NotNull Level level)
+            {
+                message = prefix + message;
+                if (level == Level.ERROR) {
+                    System.err.println(message);
+                } else {
+                    System.out.println(message);
+                }
+            }
+
+            @Override
+            public void log(@NotNull Object receiver, @NotNull String message)
+            {
+                log(message, Level.INFO);
+            }
+        };
+    }
+
     /**
      * @return The prefix applied to the start of every message.
      */
