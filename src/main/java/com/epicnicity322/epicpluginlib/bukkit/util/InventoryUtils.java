bac6a41c13897dd0a1ab863552aec5c419b09bd5
Epicnicity322 <christianorangel322@gmail.com>
2023-03-06 14:33:38 -0300

    Add InventoryUtils

---
+++ src/main/java/com/epicnicity322/epicpluginlib/bukkit/util/InventoryUtils.java
@@ -0,0 +1,261 @@
+/*
+ * EpicPluginLib - Library with basic utilities for bukkit plugins.
+ * Copyright (C) 2023  Christiano Rangel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.epicnicity322.epicpluginlib.bukkit.util;
+
+import com.epicnicity322.epicpluginlib.bukkit.EpicPluginLibBukkit;
+import com.epicnicity322.epicpluginlib.bukkit.lang.MessageSender;
+import com.epicnicity322.yamlhandler.Configuration;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.*;
+import java.util.function.Consumer;
+
+public final class InventoryUtils
+{
+    private static final @NotNull HashMap<UUID, Map<Integer, Consumer<InventoryClickEvent>>> openInventories = new HashMap<>();
+    private static final @NotNull HashMap<UUID, Consumer<InventoryCloseEvent>> onClose = new HashMap<>();
+    private static final @NotNull Listener inventoryListener = new Listener()
+    {
+        @EventHandler(priority = EventPriority.LOWEST)
+        public void onInventoryClick(InventoryClickEvent event)
+        {
+            if (event.getClickedInventory() == null) return;
+
+            HumanEntity player = event.getWhoClicked();
+            Map<Integer, Consumer<InventoryClickEvent>> buttons = openInventories.get(player.getUniqueId());
+
+            if (buttons == null) return;
+
+            event.setCancelled(true);
+
+            Consumer<InventoryClickEvent> button = buttons.get(event.getRawSlot());
+
+            if (button != null) {
+                Bukkit.getScheduler().runTask(EpicPluginLibBukkit.getInstance(), () -> {
+                    try {
+                        button.accept(event);
+                    } catch (Throwable t) {
+                        EpicPluginLibBukkit.logger().log("Failed to accept GUI click:");
+                        t.printStackTrace();
+                    }
+                });
+            }
+        }
+
+        @EventHandler
+        public void onInventoryClose(InventoryCloseEvent event)
+        {
+            HumanEntity player = event.getPlayer();
+
+            if (openInventories.remove(player.getUniqueId()) != null) {
+                if (openInventories.isEmpty()) HandlerList.unregisterAll(this);
+
+                Consumer<InventoryCloseEvent> runnable = onClose.remove(player.getUniqueId());
+
+                if (runnable != null)
+                    Bukkit.getScheduler().runTask(EpicPluginLibBukkit.getInstance(), () -> {
+                        try {
+                            runnable.accept(event);
+                        } catch (Throwable t) {
+                            EpicPluginLibBukkit.logger().log("Failed to accept GUI close:");
+                            t.printStackTrace();
+                        }
+                    });
+            }
+        }
+    };
+
+    private InventoryUtils()
+    {
+    }
+
+    /**
+     * Fills an inventory with the specified material, ignoring items that are not air.
+     * <p>
+     * If the material is from an item that has {@link ItemMeta}, the name is set to blank and the flag
+     * {@link ItemFlag#HIDE_ATTRIBUTES} is added.
+     * <p>
+     * If you input a index that is greater than the inventories size or lower than 0, the operation is aborted and
+     * nothing is filled.
+     *
+     * @param inventory  The inventory to fill.
+     * @param from_index The slot to start the filling.
+     * @param to_index   The slot to stop the filling.
+     * @see #forceFill(Material, Inventory, int, int)
+     */
+    public static void fill(@NotNull Material material, @NotNull Inventory inventory, int from_index, int to_index)
+    {
+        int size = inventory.getSize();
+        if (from_index < 0 || to_index < 0 || from_index >= size || to_index >= size) return;
+
+        ItemStack item = new ItemStack(material);
+        ItemMeta meta = item.getItemMeta();
+
+        if (meta != null) {
+            meta.setDisplayName(" ");
+            meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+            item.setItemMeta(meta);
+        }
+
+        for (int slot = from_index; slot <= to_index; ++slot) {
+            ItemStack previous = inventory.getItem(slot);
+
+            if (previous == null || !previous.getType().isAir()) {
+                inventory.setItem(slot, item);
+            }
+        }
+    }
+
+    /**
+     * Fills an inventory with the specified material, items that were already set in the index range are replaced.
+     * <p>
+     * If the material is from an item that has {@link ItemMeta}, the name is set to blank and the flag
+     * {@link ItemFlag#HIDE_ATTRIBUTES} is added.
+     * <p>
+     * If you input an index that is greater than the inventories size or lower than 0, the operation is aborted and
+     * nothing is filled.
+     *
+     * @param inventory  The inventory to fill.
+     * @param from_index The slot to start the filling.
+     * @param to_index   The slot to stop the filling.
+     * @see #fill(Material, Inventory, int, int)
+     */
+    public static void forceFill(@NotNull Material material, @NotNull Inventory inventory, int from_index, int to_index)
+    {
+        int size = inventory.getSize();
+        if (from_index < 0 || to_index < 0 || from_index >= size || to_index >= size) return;
+
+        ItemStack item = new ItemStack(material);
+        ItemMeta meta = item.getItemMeta();
+
+        if (meta != null) {
+            meta.setDisplayName(" ");
+            meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+            item.setItemMeta(meta);
+        }
+
+        for (int slot = from_index; slot <= to_index; ++slot) {
+            inventory.setItem(slot, item);
+        }
+    }
+
+    /**
+     * Creates an {@link ItemStack} based on the {@link Material} specified in the provided configuration.
+     * The item can be set to glowing if "Path.Glowing" is true.
+     * The item's name and lore will be got from the provided language in the path "Path.Display Name" and "Path.Lore".
+     *
+     * @param configPath The path to get material, glowing, display name and lore.
+     * @param config     The config to look for the material and glowing paths.
+     * @param lang       The language to look for display name and lore paths.
+     * @return An item stack as specified in the plugin's configuration.
+     */
+    public static @NotNull ItemStack getItemStack(@NotNull String configPath, @NotNull Configuration config, @NotNull MessageSender lang)
+    {
+        Material material = Material.matchMaterial(config.getString(configPath + ".Material").orElse("STONE"));
+        if (material == null || material.isAir()) material = Material.STONE;
+        ItemStack itemStack = new ItemStack(material);
+        ItemMeta itemMeta = itemStack.getItemMeta();
+
+        itemMeta.setDisplayName(lang.getColored(configPath + ".Display Name"));
+        itemMeta.setLore(Arrays.asList(lang.getColored(configPath + ".Lore").split("<line>")));
+
+        if (config.getBoolean(configPath + ".Glowing").orElse(false))
+            itemMeta.addEnchant(Enchantment.DURABILITY, 1, true);
+
+        itemMeta.addItemFlags(ItemFlag.values());
+
+        itemStack.setItemMeta(itemMeta);
+        return itemStack;
+    }
+
+    /**
+     * Opens an inventory that can't have its items moved/stolen.
+     * This inventory is closed when EpicPluginLib is disabled.
+     *
+     * @param inventory The inventory to open.
+     * @param player    The player to open the inventory to.
+     * @throws IllegalStateException If PlayMoreSounds is not loaded.
+     * @see #openInventory(Inventory inventory, Map buttons, HumanEntity player, Consumer onClose)
+     */
+    public static void openInventory(@NotNull Inventory inventory, @NotNull HumanEntity player)
+    {
+        openInventory(inventory, null, player, null);
+    }
+
+    /**
+     * Opens an inventory that you can map each slot number to a {@link Consumer} for {@link InventoryClickEvent}, so
+     * when the player clicks specified slot, the consumer is accepted.
+     * The items in this inventory can not be moved/stolen.
+     * This inventory is closed when EpicPluginLib is disabled.
+     *
+     * @param inventory The inventory to open.
+     * @param buttons   The map with the number of the slot that when clicked will run the {@link Runnable}.
+     * @param player    The player to open the inventory to.
+     * @throws IllegalStateException If PlayMoreSounds is not loaded.
+     * @see #openInventory(Inventory inventory, Map buttons, HumanEntity player, Consumer onClose)
+     */
+    public static void openInventory(@NotNull Inventory inventory, @NotNull Map<Integer, Consumer<InventoryClickEvent>> buttons, @NotNull HumanEntity player)
+    {
+        openInventory(inventory, buttons, player, null);
+    }
+
+    /**
+     * Opens an inventory that you can map each slot number to a {@link Consumer} for {@link InventoryClickEvent}, so
+     * when the player clicks the specified slot, the consumer is accepted. Also, a {@link Consumer} for
+     * {@link InventoryCloseEvent} is accepted when the inventory closes.
+     * The items in this inventory can not be moved/stolen.
+     * This inventory is closed when EpicPluginLib is disabled.
+     *
+     * @param inventory The inventory to open.
+     * @param buttons   The map with the number of the slot that when clicked will run the {@link Runnable}.
+     * @param player    The player to open the inventory to.
+     * @param onClose   The runnable to run when the inventory is closed.
+     * @throws IllegalStateException If PlayMoreSounds is not loaded.
+     */
+    public static void openInventory(@NotNull Inventory inventory, @Nullable Map<Integer, Consumer<InventoryClickEvent>> buttons, @NotNull HumanEntity player, @Nullable Consumer<InventoryCloseEvent> onClose)
+    {
+        EpicPluginLibBukkit instance = EpicPluginLibBukkit.getInstance();
+        if (instance == null || !instance.isEnabled()) throw new IllegalStateException("EpicPluginLib is not loaded.");
+
+        player.openInventory(inventory);
+
+        if (openInventories.isEmpty())
+            Bukkit.getPluginManager().registerEvents(inventoryListener, instance);
+
+        if (buttons == null) buttons = Collections.emptyMap();
+
+        openInventories.put(player.getUniqueId(), buttons);
+        InventoryUtils.onClose.put(player.getUniqueId(), onClose);
+    }
+}
