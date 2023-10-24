5ad2b10e080d5e2d8485c46880def6764fe00114
Epicnicity322 <christianorangel322@gmail.com>
2023-10-24 18:49:15 -0300

    Bump version 2.5.1

--- src/main/java/com/epicnicity322/epicpluginlib/core/EpicPluginLibVersion.java
+++ src/main/java/com/epicnicity322/epicpluginlib/core/EpicPluginLibVersion.java
@@ -22,5 +22,5 @@ import org.jetbrains.annotations.NotNull;
 
 class EpicPluginLibVersion
 {
-    static final @NotNull String version = "2.5";
+    static final @NotNull String version = "2.5.1";
 }
